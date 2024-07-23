80b5ccbd2bf37f50b025a1377c4fcc06dbe616e2
chilln <kimjihan77@naver.com>
2024-07-23 11:16:23 +0900

    1.5 특정문자 뒤집기

---
+++ currinculum/String/특정문자뒤집기/Main.java
@@ -0,0 +1,32 @@
+package String.특정문자뒤집기;
+import java.util.Scanner;
+
+class Main{
+    public String solution(String str){
+        String answer;
+        char[] chars = str.toCharArray();
+        int lt = 0, rt = str.length()-1;
+
+        while(lt < rt){
+            if(!Character.isAlphabetic(chars[lt])) lt ++;
+            else if(!Character.isAlphabetic(chars[rt])) rt --;
+            else {
+                char tmp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = tmp;
+                lt ++;
+                rt --;
+            }
+        }
+
+        answer = String.valueOf(chars);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main M = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(M.solution(str));
+    }
+}
